4c8e9cd7f311ec73d7b9c80e2fe6ffcbddbbe4e9
bertintshisuaka2 <bertintshisuaka@hotmail.com>
2023-03-10 12:47:42 -0500

    Finished server.js and config

--- paradise-app/server/server.js
+++ paradise-app/server/server.js
@@ -39,3 +39,24 @@
 
 
 //this is what is needed in the server for this project
+const express = require('express');
+const dbConnection = require('./config/connection');
+const routes = require('./routes');
+
+//set up server and the port number
+const app = express();
+const port = process.env.PORT || 3001;
+
+//allowing the use of the data from response
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//calling the routes; url here is localhost:3001
+app.use(routes);
+
+//connecting to mongoDB and starting the server
+dbConnection.once('open', () => {
+    app.listen(port, () => {
+        console.log(`Server starting on port ${port}....Now listening....`);
+    });
+});
